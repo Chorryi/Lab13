d170542fefeff2457dfcd64a0057eef1f4049c7f
Chorryi <lennestir@gmail.com>
2015-02-24 11:01:36 -0500

    Update B.java

--- B.java
+++ B.java
@@ -1,4 +1,38 @@
+
 public class B extends Terminals implements Lounge
 {
-    //fill this in
+    public String getPlanes()
+    {
+        return "Planes";
+    }
+    
+    public String getGates()
+    {
+        return "Gates";
+    }
+    
+    public String getSchedule()
+    {
+        return "Schedule";
+    }
+    
+    public String getRestruants()
+    {
+        return "Restruants";
+    }
+    
+    public String getBathrooms()
+    {
+        return "Bathrooms are to the left";
+    }
+    
+        public int getSeats()
+    {
+        return 50;
+    }
+    
+    public String getDrinks()
+    {
+        return "test";
+    }
 }

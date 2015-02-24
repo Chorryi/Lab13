2bd9a7e35b21975ce3daf0c7af0cd4e46c0e9f41
Chorryi <lennestir@gmail.com>
2015-02-24 11:01:52 -0500

    Update C.java

--- C.java
+++ C.java
@@ -1,4 +1,39 @@
-public class C extends Terminals implements Lounge
+
+/**
+ * Write a description of class C here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class C extends Lobby implements Lounge
 {
-    //fill this in
+    public String getTickets()
+    {
+        return "Tickets";
+    }
+    
+    public String getLuggage()
+    {
+        return "Luggage";
+    }
+    
+    public String getConcession()
+    {
+        return "Concession Stands are to the right";
+    }
+    
+    public String getBathrooms()
+    {
+        return "Bathrooms are to the left";
+    }
+    
+    public int getSeats()
+    {
+        return 50;
+    }
+    
+    public String getDrinks()
+    {
+        return "test";
+    }
 }
